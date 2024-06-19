56481dc9b118b6b214b1d2c88b96ff5075200bf0
it1shka <2tishbel@gmail.com>
2024-06-19 16:49:14 +0200

    Continue to do day 7

--- day7/task1.zig
+++ day7/task1.zig
@@ -13,7 +13,7 @@ const Token = struct {
   kind: TokenType,
   span: []const u8,
 
-  fn init(allocator: std.mem.Allocator, kind: TokenType, span: []const u8) !Token {
+  fn init(allocator: std.mem.Allocator, kind: TokenType, span: []const u8) !@This() {
     return @This() {
       .kind = kind,
       .span = try allocator.dupe(u8, span),
@@ -44,6 +44,49 @@ fn cleanTokens(allocator: std.mem.Allocator, dest: *std.ArrayList(Token)) void {
   }
   dest.clearAndFree();
 }
+
+const LogicGate = struct {
+  left: []const u8,
+  right: []const u8,
+
+  fn init(allocator: std.mem.Allocator, left: []const u8, right: []const u8) !@This() {
+    return @This() {
+      .left = try allocator.dupe(u8, left),
+      .right = try allocator.dupe(u8, right),
+    };
+  }
+
+  fn deinit(self: *@This(), allocator: std.mem.Allocator) void {
+    allocator.free(self.left);
+    allocator.free(self.right);
+  }
+};
+
+const ShiftGate = struct {
+  wire: []const u8,
+  shift: u32,
+
+  fn init(allocator: std.mem.Allocator, wire: []const u8, shift: u32) !@This() {
+    return @This() {
+      .wire = try allocator.dupe(wire),
+      .shift = shift,
+    };
+  }
+
+  fn deinit(self: *@This(), allocator: std.mem.Allocator) void {
+    allocator.free(self.wire);
+  }
+};
+
+const WireInput = union(enum) {
+  constant: u16,
+  andGate: LogicGate,
+  orGate: LogicGate,
+  lshiftGate: ShiftGate,
+  rshiftGate: ShiftGate,
+  notGate: []const u8,
+  // TODO: fn parse() and fn deinit()
+};
 
 pub fn main() !void {
   var file = try std.fs.cwd().openFile("input.txt", .{});
