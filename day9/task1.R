5130ff0066d56ad54d7074323d3e4a84bb32e615
it1shka <2tishbel@gmail.com>
2024-08-06 17:53:55 +0200

    Added solution to day9 in R

--- day9/task1.R
+++ day9/task1.R
@@ -1,5 +1,9 @@
 library(tidyverse)
+library(foreach)
+library(doParallel)
 library(combinat)
+
+registerDoParallel(cores = detectCores())
 
 read_input <- function() {
   rows <-
@@ -17,7 +21,40 @@ read_input <- function() {
     mutate(distance = as.integer(distance))
 }
 
+get_routes <- function(frame) {
+  c(input$from, input$to) |>
+    unique() |>
+    permn() %>%
+    do.call(rbind, .) |>
+    as_tibble()
+}
+
+get_weighted_routes <- function(distances, routes) {
+  route_dist <- foreach(i = seq_len(nrow(routes)), .combine = "c") %dopar% {
+    paste0("Started #", i) |> print()
+    total <- 0
+    route <- routes[i, ]
+    for (j in seq_len(length(route) - 1)) {
+      a <- route[[j]]
+      b <- route[[j + 1]]
+      distance <- distances |>
+        filter((from == a & to == b) | (from == b & to == a)) |>
+        select(distance) |>
+        unlist()
+      total <- total + distance
+    }
+    paste0("Distance #", i, " is ", total) |> print()
+    total
+  }
+  routes |>
+    mutate(distance = route_dist)
+}
+
 input <- read_input()
-cities <- c(input$from, input$to) |> unique()
-routes <- permn(cities)
-head(routes)
+routes <- get_routes(input)
+weighted_routes <- get_weighted_routes(input, routes)
+min_distance <-
+  weighted_routes |>
+  select(distance) |>
+  min()
+min_distance
